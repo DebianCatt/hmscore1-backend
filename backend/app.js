01c2c7d8d307e88dd7126cdd86392ad1a99b6f55
john paul oco <johnpauloco435@gmail.com>
2024-11-24 04:36:23 +0800

    last test app vercel 8

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const app = express();
 //config({path: "./config/config.env"});
 
 app.use(cors({
-    origin: "*",
+    origin: [process.env.DASHBOARD_URL || "*"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));

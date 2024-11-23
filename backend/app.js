bd25dc88710746cbfc7128b26a658e1c0d799932
john paul oco <johnpauloco435@gmail.com>
2024-11-24 04:24:18 +0800

    roll

--- backend/app.js
+++ backend/app.js
@@ -4,48 +4,58 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import { dbConnection } from "./database/dbConnection.js";
-import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+import {errorMiddleware} from "./middlewares/errorMiddleware.js";
 import userRouter from "./router/userRouter.js";
+//import appointmentRouter from "./router/appointmentRouter.js";
 import inpatientRouter from "./router/inpatientRouter.js";
 import outpatientRouter from "./router/outpatientRouter.js";
 import archivedRouter from "./router/archivedRouter.js";
+//import { updateInpatient } from "./controller/inpatientController.js";
+
 
 
 const app = express();
+//config({path: "./config/config.env"});
 
 app.use(cors({
-    origin: [
-      process.env.DASHBOARD_URL, // Frontend URL in .env
-      "https://core1.nodadogenhospital.com", // Frontend URL directly added for CORS
-      "https://hmscore1-backend.vercel.app", // Vercel Backend URL
-    ],
+    origin: [process.env.DASHBOARD_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, // Allow cookies if needed
-  }));
-  
+    credentials: true,
+}));
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 app.use(fileUpload({
-  useTempFiles: true,
-  tempFileDir: "/tmp/",
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
 }));
 
+
+//app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
+//app.use("/api/v1/appointment", appointmentRouter);
 app.use("/api/v1/inpatients", inpatientRouter);
 app.use("/api/v1/outpatients", outpatientRouter);
 app.use("/api/v1/archivedPatients", archivedRouter);
 app.use("/api/v1/updateInpatients", inpatientRouter);
 
-// Test route to check if the backend is working
+//test route
 app.get("/test", (req, res) => {
-  res.status(200).send("Backend is running!");
+    res.status(200).send("Backend is running!");
 });
+
+
 
 dbConnection();
 
 app.use(errorMiddleware);
 
 export default app;
+
+
+
+
+
+

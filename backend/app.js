592f0ea3a91f22a690b74f759ff1399cc2257da6
john paul oco <johnpauloco435@gmail.com>
2024-11-24 04:03:48 +0800

    troubleshoot app

--- backend/app.js
+++ backend/app.js
@@ -4,58 +4,43 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import { dbConnection } from "./database/dbConnection.js";
-import {errorMiddleware} from "./middlewares/errorMiddleware.js";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import userRouter from "./router/userRouter.js";
-//import appointmentRouter from "./router/appointmentRouter.js";
 import inpatientRouter from "./router/inpatientRouter.js";
 import outpatientRouter from "./router/outpatientRouter.js";
 import archivedRouter from "./router/archivedRouter.js";
-//import { updateInpatient } from "./controller/inpatientController.js";
-
 
 
 const app = express();
-config({path: "./config/config.env"});
 
 app.use(cors({
-    origin: [process.env.DASHBOARD_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+  origin: [process.env.DASHBOARD_URL],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
 }));
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(fileUpload({
-    useTempFiles: true,
-    tempFileDir: "/tmp/",
+  useTempFiles: true,
+  tempFileDir: "/tmp/",
 }));
 
-
-//app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
-//app.use("/api/v1/appointment", appointmentRouter);
 app.use("/api/v1/inpatients", inpatientRouter);
 app.use("/api/v1/outpatients", outpatientRouter);
 app.use("/api/v1/archivedPatients", archivedRouter);
 app.use("/api/v1/updateInpatients", inpatientRouter);
 
-//test route
+// Test route to check if the backend is working
 app.get("/test", (req, res) => {
-    res.status(200).send("Backend is running!");
+  res.status(200).send("Backend is running!");
 });
-
-
 
 dbConnection();
 
 app.use(errorMiddleware);
 
 export default app;
-
-
-
-
-
-

0438ca4a36264507294d3bb096efce2e1947b078
john paul oco <johnpauloco435@gmail.com>
2024-11-24 17:16:46 +0800

    app test 11

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const app = express();
 //config({path: "./config/config.env"});
 
 app.use(cors({
-    origin: process.env.DASHBOARD_URL || "https://core1.nodadogenhospital.com",
+    origin: [process.env.DASHBOARD_URL || "https://core1.nodadogenhospital.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));

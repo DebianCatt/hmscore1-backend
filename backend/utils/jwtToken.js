149ac4b6460e553ed2fc7514df5f30e229c615a1
john paul oco <johnpauloco435@gmail.com>
2024-11-24 16:49:49 +0800

    token upd2

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -7,7 +7,7 @@ export const generateToken = (user, message, statusCode, res) => {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        domain: 'core1.nodadogenhospital.com',
+        domain: '.nodadogenhospital.com',
     }).json({
         success: true,
         message,
